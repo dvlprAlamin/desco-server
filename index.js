1386abbe25c3ea688203afb2a8817e769450ee6a
dvlprAlamin <alaminhowladerofficial@gmail.com>
2022-01-07 21:13:35 +0600

    added login signup & jwt

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 
@@ -10,6 +12,17 @@ app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsgsy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = await req.headers.authorization.split('Bearer ')[1];
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(user);
+        req.decodedEmail = user.email;
+    }
+    next();
+}
+
 
 const formValidation = data => {
     let message = ''
@@ -29,9 +42,13 @@ async function run() {
         await client.connect();
         const database = client.db(process.env.DB_NAME);
         const billingCollection = database.collection('billing-list');
+        const userCollection = database.collection('user');
 
         // Get Billing data API
-        app.get('/api/billing-list', async (req, res) => {
+        app.get('/api/billing-list', verifyToken, async (req, res) => {
+            // verifyToken
+            const validUser = await userCollection.findOne({ email: 'alamin' });
+            console.log('kisu', validUser);
             const cursor = billingCollection.find({});
             const sorted = cursor.sort({ _id: -1 })
             const page = req.query.page;
@@ -42,7 +59,7 @@ async function run() {
                 bills = await sorted.skip(page * size).limit(size).toArray();
             }
             else {
-                bills = await cursor.toArray();
+                bills = await sorted.toArray();
             }
             res.send(bills);
             // res.send({ count, bills });
@@ -61,19 +78,57 @@ async function run() {
         })
         //Update bills API
         app.put('/api/update-billing/:id', async (req, res) => {
-            const id = ObjectId(req.params.id);
-            const data = req.body;
-            const filter = { _id: id };
-            const options = { upsert: true };
-            const updateDoc = { $set: data };
-            const result = await billingCollection.updateOne(filter, updateDoc, options);
-            res.json(result.modifiedCount > 0);
+            const invalidMessage = formValidation(req.body);
+            if (!invalidMessage) {
+                const id = ObjectId(req.params.id);
+                const data = req.body;
+                const filter = { _id: id };
+                const options = { upsert: true };
+                const updateDoc = { $set: data };
+                const result = await billingCollection.updateOne(filter, updateDoc, options);
+                res.json(result.modifiedCount > 0);
+            } else {
+                res.status(400).send(invalidMessage)
+            }
+
         })
         // Delete bills API
         app.delete('/api/delete-billing/:id', async (req, res) => {
             const id = ObjectId(req.params.id);
             const result = await billingCollection.findOneAndDelete({ _id: id })
             res.send(result.ok > 0)
+        })
+
+        // User Register API
+        app.post('/api/registration', async (req, res) => {
+            const { name, email, password } = req.body;
+            const cursor = userCollection.find({ email })
+            const isExist = await cursor.toArray();
+            if (isExist.length > 0) {
+                res.status(409).send('Email already exists')
+            } else {
+                const encryptPass = await bcrypt.hash(password, 10);
+                const result = await userCollection.insertOne({ name, email, password: encryptPass });
+                res.json(result)
+            }
+        })
+
+        // User Login API
+        app.post('/api/login', async (req, res) => {
+            const { email, password } = req.body;
+            const user = await userCollection.findOne({ email })
+            if (!user) {
+                return res.status(400).json({ message: 'Invalid Email' })
+            }
+            if (await bcrypt.compare(password, user.password)) {
+                const token = jwt.sign({
+                    id: user._id,
+                    email: user.email
+                }, process.env.JWT_SECRET)
+                res.status(200).send({ status: true, token: token, email: user.email })
+            } else {
+                res.status(400).send({ status: false, message: "Password didn't match" })
+            }
         })
 
     }
